8492163384a93d17c5fd24ddcaae3f53fccdf0dd
Elliott Mahler <join.together@gmail.com>
2019-12-01 22:04:56 -0800

    Implemented some rendering options

--- src/gui.rs
+++ src/gui.rs
@@ -1,17 +1,20 @@
 
 use crate::common::FractalResult;
 use crate::compute;
+use crate::render;
+use crate::render::ColorChannel;
 use std::sync::Arc;
 
 use iced::{
-    button, image, text_input,
-    Application, Background, Button, Color, Column, Command, Container, Element, HorizontalAlignment, Image, Length, Row, Text, TextInput,
+    button, image, slider, text_input, 
+    Application, Background, Button, Color, Column, Command, Container, Element, HorizontalAlignment, Image, Length, Row, Slider, Text, TextInput,
 };
 
 #[derive(Default)]
 pub struct FractalGUI {
     ui_state: UIData,
     compute_params: compute::ComputeParams,
+    render_params: render::RenderParams,
     fractal_data: Option<Arc<FractalResult>>,
     fractal_image: Option<image::Handle>,
     state: State,
@@ -33,15 +36,36 @@ impl Default for State {
 #[derive(Debug, Clone)]
 pub enum Message {
     InitialSizeChanged(String),
+    ColorChanged(SliderColor, ColorChannel, f32),
     BeginComputingFractal,
     FractalComputed(Arc<FractalResult>),
     FractalRendered(image::Handle),
+}
+
+#[derive(Debug, Clone)]
+pub enum SliderColor {
+    Color0,
+    Color1,
+    Color2,
+    Color3,
 }
 
 #[derive(Default)]
 struct UIData {
     compute_button: button::State,
     initial_size_text: text_input::State,
+    background_color_red_slider: slider::State,
+    background_color_green_slider: slider::State,
+    background_color_blue_slider: slider::State,
+    color1_red_slider: slider::State,
+    color1_green_slider: slider::State,
+    color1_blue_slider: slider::State,
+    color2_red_slider: slider::State,
+    color2_green_slider: slider::State,
+    color2_blue_slider: slider::State,
+    color3_red_slider: slider::State,
+    color3_green_slider: slider::State,
+    color3_blue_slider: slider::State,
 }
 
 impl Application for FractalGUI {
@@ -61,6 +85,24 @@ impl Application for FractalGUI {
                 self.compute_params.initial_size = value;
                 Command::none()
             },
+            Message::ColorChanged(which_color, channel, value) => {
+                match which_color {
+                    SliderColor::Color0 => self.render_params.color0.set_normalized(channel, value),
+                    SliderColor::Color1 => self.render_params.color1.set_normalized(channel, value),
+                    SliderColor::Color2 => self.render_params.color2.set_normalized(channel, value),
+                    SliderColor::Color3 => self.render_params.color3.set_normalized(channel, value),
+                }
+                if let Some(data) = &self.fractal_data {
+                    if self.state == State::Idle {
+                        Command::perform(render::render_fractal(self.render_params.clone(), Arc::clone(data)), Message::FractalRendered)
+                    } else {
+                        Command::none()
+                    }
+                }
+                else {
+                    Command::none()
+                }
+            },
             Message::BeginComputingFractal => {
                 self.state = State::Computing;
                 Command::perform(compute::compute_fractal(self.compute_params.clone()), Message::FractalComputed)
@@ -68,7 +110,7 @@ impl Application for FractalGUI {
             Message::FractalComputed(result) => {
                 self.fractal_data = Some(Arc::clone(&result));
                 self.state = State::Rendering;
-                Command::perform(crate::render::render_fractal(result), Message::FractalRendered)
+                Command::perform(render::render_fractal(self.render_params.clone(), result), Message::FractalRendered)
             }
             Message::FractalRendered(result) => {
                 self.fractal_image = Some(result);
@@ -82,6 +124,7 @@ impl Application for FractalGUI {
         let FractalGUI {
             ui_state,
             compute_params,
+            render_params,
             fractal_data: _,
             fractal_image,
             state,
@@ -92,7 +135,7 @@ impl Application for FractalGUI {
         let initial_size_text = TextInput::new(
             &mut ui_state.initial_size_text,
             "Initial Count",
-            &mut compute_params.initial_size,
+            &compute_params.initial_size,
             |mut value| {
                 value.retain(|c| c.is_digit(10));
                 Message::InitialSizeChanged(value)
@@ -106,7 +149,7 @@ impl Application for FractalGUI {
             .width(Length::Fill)
             .spacing(20)
             .push(Column::new()
-                .width(Length::Units(200))
+                .width(Length::Fill)
                 .spacing(10)
                 .push(Text::new("Initial Count")
                     .color([0.1, 0.1, 0.1])
@@ -115,6 +158,62 @@ impl Application for FractalGUI {
                 .push(initial_size_text)
                 .push(
                     button(&mut ui_state.compute_button, "Compute", *state == State::Idle, Message::BeginComputingFractal),
+                )
+                .push(Text::new("Background Color")
+                    .color([0.1, 0.1, 0.1])
+                    .horizontal_alignment(HorizontalAlignment::Center)
+                )
+                .push(Row::new()
+                    .width(Length::Fill)
+                    .spacing(5)
+                    .push(Text::new("R").color([0.1, 0.1, 0.1]).horizontal_alignment(HorizontalAlignment::Right))
+                    .push(Slider::new(&mut ui_state.background_color_red_slider, 0.0..=1.0, render_params.color0.get_normalized(ColorChannel::Red), |val| Message::ColorChanged(SliderColor::Color0, ColorChannel::Red, val)))
+                    .push(Text::new("G").color([0.1, 0.1, 0.1]).horizontal_alignment(HorizontalAlignment::Right))
+                    .push(Slider::new(&mut ui_state.background_color_green_slider, 0.0..=1.0, render_params.color0.get_normalized(ColorChannel::Green), |val| Message::ColorChanged(SliderColor::Color0, ColorChannel::Green, val)))
+                    .push(Text::new("B").color([0.1, 0.1, 0.1]).horizontal_alignment(HorizontalAlignment::Right))
+                    .push(Slider::new(&mut ui_state.background_color_blue_slider, 0.0..=1.0, render_params.color0.get_normalized(ColorChannel::Blue), |val| Message::ColorChanged(SliderColor::Color0, ColorChannel::Blue, val)))
+                )
+                .push(Text::new("Color (Value=1)")
+                    .color([0.1, 0.1, 0.1])
+                    .horizontal_alignment(HorizontalAlignment::Center)
+                )
+                .push(Row::new()
+                    .width(Length::Fill)
+                    .spacing(5)
+                    .push(Text::new("R").color([0.1, 0.1, 0.1]).horizontal_alignment(HorizontalAlignment::Right))
+                    .push(Slider::new(&mut ui_state.color1_red_slider, 0.0..=1.0, render_params.color1.get_normalized(ColorChannel::Red), |val| Message::ColorChanged(SliderColor::Color1, ColorChannel::Red, val)))
+                    .push(Text::new("G").color([0.1, 0.1, 0.1]).horizontal_alignment(HorizontalAlignment::Right))
+                    .push(Slider::new(&mut ui_state.color1_green_slider, 0.0..=1.0, render_params.color1.get_normalized(ColorChannel::Green), |val| Message::ColorChanged(SliderColor::Color1, ColorChannel::Green, val)))
+                    .push(Text::new("B").color([0.1, 0.1, 0.1]).horizontal_alignment(HorizontalAlignment::Right))
+                    .push(Slider::new(&mut ui_state.color1_blue_slider, 0.0..=1.0, render_params.color1.get_normalized(ColorChannel::Blue), |val| Message::ColorChanged(SliderColor::Color1, ColorChannel::Blue, val)))
+                )
+                .push(Text::new("Color (Value=2)")
+                    .color([0.1, 0.1, 0.1])
+                    .horizontal_alignment(HorizontalAlignment::Center)
+                )
+                .push(Row::new()
+                    .width(Length::Fill)
+                    .spacing(5)
+                    .push(Text::new("R").color([0.1, 0.1, 0.1]).horizontal_alignment(HorizontalAlignment::Right))
+                    .push(Slider::new(&mut ui_state.color2_red_slider, 0.0..=1.0, render_params.color2.get_normalized(ColorChannel::Red), |val| Message::ColorChanged(SliderColor::Color2, ColorChannel::Red, val)))
+                    .push(Text::new("G").color([0.1, 0.1, 0.1]).horizontal_alignment(HorizontalAlignment::Right))
+                    .push(Slider::new(&mut ui_state.color2_green_slider, 0.0..=1.0, render_params.color2.get_normalized(ColorChannel::Green), |val| Message::ColorChanged(SliderColor::Color2, ColorChannel::Green, val)))
+                    .push(Text::new("B").color([0.1, 0.1, 0.1]).horizontal_alignment(HorizontalAlignment::Right))
+                    .push(Slider::new(&mut ui_state.color2_blue_slider, 0.0..=1.0, render_params.color2.get_normalized(ColorChannel::Blue), |val| Message::ColorChanged(SliderColor::Color2, ColorChannel::Blue, val)))
+                )
+                .push(Text::new("Color (Value=3)")
+                    .color([0.1, 0.1, 0.1])
+                    .horizontal_alignment(HorizontalAlignment::Center)
+                )
+                .push(Row::new()
+                    .width(Length::Fill)
+                    .spacing(5)
+                    .push(Text::new("R").color([0.1, 0.1, 0.1]).horizontal_alignment(HorizontalAlignment::Right))
+                    .push(Slider::new(&mut ui_state.color3_red_slider, 0.0..=1.0, render_params.color3.get_normalized(ColorChannel::Red), |val| Message::ColorChanged(SliderColor::Color3, ColorChannel::Red, val)))
+                    .push(Text::new("G").color([0.1, 0.1, 0.1]).horizontal_alignment(HorizontalAlignment::Right))
+                    .push(Slider::new(&mut ui_state.color3_green_slider, 0.0..=1.0, render_params.color3.get_normalized(ColorChannel::Green), |val| Message::ColorChanged(SliderColor::Color3, ColorChannel::Green, val)))
+                    .push(Text::new("B").color([0.1, 0.1, 0.1]).horizontal_alignment(HorizontalAlignment::Right))
+                    .push(Slider::new(&mut ui_state.color3_blue_slider, 0.0..=1.0, render_params.color3.get_normalized(ColorChannel::Blue), |val| Message::ColorChanged(SliderColor::Color3, ColorChannel::Blue, val)))
                 )
             );
 
--- src/render.rs
+++ src/render.rs
@@ -1,28 +1,73 @@
 
-use image::{DynamicImage, ImageBuffer, RgbImage, ImageOutputFormat};
+use image::{DynamicImage, ImageBuffer, ImageOutputFormat};
+use image::math::utils::clamp;
 use iced::image::Handle;
 use std::io::Cursor;
 use std::sync::Arc;
 use crate::common::FractalResult;
 
-
-pub async fn render_fractal(fractal_data: Arc<FractalResult>) -> Handle {
-    let result_image = DynamicImage::ImageRgb8(create_fractal_image(&fractal_data.sand_data, &fractal_data.count_data, fractal_data.side_length));
-    let mut cursor = Cursor::new(Vec::new());
-    result_image.write_to(&mut cursor, ImageOutputFormat::PNG).expect("Failed to encode image data to memory");
-    Handle::from_bytes(cursor.into_inner())
+#[derive(Clone, Debug)]
+pub enum ColorChannel {
+	Red,
+	Green,
+	Blue
 }
 
-fn create_fractal_image(input_data: &[u8], counting_data: &[u32], side_length: usize) -> RgbImage {
-	let mut data_img = ImageBuffer::new(side_length as u32, side_length as u32);
-	for ((_,_, pixel), (fractal_value, _)) in data_img.enumerate_pixels_mut().zip(input_data.iter().zip(counting_data)) {
-		*pixel = match fractal_value {
-			0 => image::Rgb([0,0,0]),
-			1 => image::Rgb([64,64,255]),
-			2 => image::Rgb([255,255,64]),
-			_ => image::Rgb([255,64,64]),
+#[derive(Clone, Debug)]
+pub struct RenderColor(image::Rgb<u8>);
+
+impl RenderColor {
+	pub fn get_normalized(&self, channel: ColorChannel) -> f32 {
+		let value = match channel {
+			ColorChannel::Red => self.0[0],
+			ColorChannel::Green => self.0[1],
+			ColorChannel::Blue => self.0[2],
+		};
+		f32::from(value) / 255.0
+	}
+	pub fn set_normalized(&mut self, channel: ColorChannel, value: f32) {
+		let clamped_value = clamp(value * 255.0, 0.0, 255.0);
+		let converted_value = clamped_value as u8;
+		match channel {
+			ColorChannel::Red => self.0[0] = converted_value,
+			ColorChannel::Green => self.0[1] = converted_value,
+			ColorChannel::Blue => self.0[2] = converted_value,
 		}
 	}
-	data_img
+}
+
+#[derive(Clone, Debug)]
+pub struct RenderParams {
+	pub color0: RenderColor,
+	pub color1: RenderColor,
+	pub color2: RenderColor,
+	pub color3: RenderColor,
+}
+
+impl Default for RenderParams {
+    fn default() -> Self {
+        Self {
+            color0: RenderColor(image::Rgb([0,0,0])),
+            color1: RenderColor(image::Rgb([64,64,255])),
+            color2: RenderColor(image::Rgb([255,255,64])),
+            color3: RenderColor(image::Rgb([255,64,64])),
+        }
+    }
+}
+
+
+pub async fn render_fractal(params: RenderParams, fractal_data: Arc<FractalResult>) -> Handle {
+    let mut data_img = ImageBuffer::new(fractal_data.side_length as u32, fractal_data.side_length as u32);
+	for ((_,_, pixel), (fractal_value, _)) in data_img.enumerate_pixels_mut().zip(fractal_data.sand_data.iter().zip(fractal_data.count_data.iter())) {
+		*pixel = match fractal_value {
+			0 => params.color0.0,
+			1 => params.color1.0,
+			2 => params.color2.0,
+			_ => params.color3.0,
+		}
+	}
+    let mut cursor = Cursor::new(Vec::new());
+    DynamicImage::ImageRgb8(data_img).write_to(&mut cursor, ImageOutputFormat::PNG).expect("Failed to encode image data to memory");
+    Handle::from_bytes(cursor.into_inner())
 }
 
